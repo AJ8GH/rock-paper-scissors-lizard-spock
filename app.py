6e5b0c9101c4344fd0cb0f9f882104b8943ac63e
Adam Jonas <adamjonas25@gmail.com>
2021-05-19 21:53:44 +0100

    Hard code name in play view

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request, session
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/play', methods = ['POST'])
+def new():
+    # session['player'] = request.form['name']
+    return redirect(url_for('play'))
+
+@app.route('/play')
+def play():
+    # player = session['player']
+    return render_template('play.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- test/home_page_test.py
+++ test/home_page_test.py
@@ -7,5 +7,13 @@ class HomePageTest(unittest.TestCase):
         browser.visit('http://localhost:5000')
         assert browser.is_text_present('Rock Paper Scissors Lizard Spock')
 
+    def test_entering_name(self):
+        browser = Browser()
+        browser.visit('http://localhost:5000')
+        browser.fill('name', 'test name')
+        button = browser.find_by_name('play')
+        button.click()
+        assert browser.is_text_present('Welcome test name!')
+
 if __name__ == '__main__':
     unittest.main()
