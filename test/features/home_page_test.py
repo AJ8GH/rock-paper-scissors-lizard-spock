782b11b3450edac13171b8adf4f7f611160b8371
Adam Jonas <adamjonas25@gmail.com>
2021-05-22 17:45:53 +0100

    Remove chrome driver from feature tests

--- test/features/home_page_test.py
+++ test/features/home_page_test.py
@@ -10,7 +10,7 @@ class HomePageTest(unittest.TestCase):
         assert browser.is_text_present('Rock Paper Scissors Lizard Spock')
 
     def test_entering_name(self):
-        browser = Browser('chrome', headless=True)
+        browser = Browser(headless=True)
         browser.visit('http://localhost:5000')
         browser.fill('name', 'test name')
         button = browser.find_by_name('play')
