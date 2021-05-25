86099bf8787cfec99d78e248072069cdd6f942b4
Adam Jonas <adamjonas25@gmail.com>
2021-05-25 10:52:56 +0100

    Add test for index route

--- app/app_test.py
+++ app/app_test.py
@@ -5,8 +5,18 @@
 
 # app.testing = True
 
-# class AppTest(unittest.TestCase):
-#     def test_index_route_exists(self):
-#         with app.test_client() as c:
-#             response = c.get('/')
-#             self.assertEquals(response.status_code, 200)
+
+import pytest
+
+from app import app
+
+
+@pytest.fixture
+def client():
+    app.config["TESTING"] = True
+    with app.test_client() as client:
+        yield client
+
+def test_index_route_exists(client):
+    response = client.get('/')
+    assert response.status_code == 200
